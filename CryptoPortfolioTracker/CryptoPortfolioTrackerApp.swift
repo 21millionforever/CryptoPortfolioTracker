d8ef66746e551633cae9a760c0a1af8ca6c517d4
21millionforever <chen86349@gmail.com>
2023-12-25 16:10:54 -0500

    Initial Commit

---
+++ CryptoPortfolioTracker/CryptoPortfolioTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioTrackerApp.swift
+//  CryptoPortfolioTracker
+//
+//  Created by Zhendong Chen on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
